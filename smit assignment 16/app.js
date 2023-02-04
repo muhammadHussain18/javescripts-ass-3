414eb951d0269f07ea656b977c1ab0372c839ac8
Muhammad Hussain <122493431+muhammadHussain18@users.noreply.github.com>
2023-02-04 21:26:55 +0500

    smit assignment 3

---
+++ smit assignment 16/app.js
@@ -0,0 +1,190 @@
+
+
+// // Qno#1
+
+// var num1 = 3
+// var num2 = 5
+// document.write(num1 + num2 + "<br />")
+
+
+// // Qno#2
+
+// document.write(num1 - num2 + "<br />")
+// document.write(num1 * num2 + "<br />")
+// document.write(num1 / num2 + "<br />")
+// document.write(num1 % num2 + "<br />")
+
+
+// // Qno#3
+
+// var num3 = 
+// document.write("value after variable declarition is" + " " +  num3 + "<br />")
+// num3 = 5
+// document.write("initial value: " + " " + num3  + "<br />")
+// num3 = ++num3
+// document.write("value after increment is :" + " " + num3 + "<br />")
+// num3 = num3 + 7
+// document.write( "value after addition is :" + " " + num3  + "<br />")
+// num3 = --num3
+// document.write("value after decrement is :" + " " + num3  + "<br />")
+// num3 = num3 % 3
+// document.write( "the reminder is : "  + " " + num3  + "<br />")
+
+
+// // Qno#4
+
+// var ticket = 600
+// var quantity = 5 
+// var result = 600*5
+// // console.log(result)
+// document.write("Total cost to buy" + " " + quantity + " " + "tickets to a movie is " + result + "PKR" )
+
+
+// // Qno#5
+
+// var tab = 4
+// var num=1
+// var result= tab*num
+// document.write("Table of " + " " + tab + "<br />")
+// document.write(tab + " x " + num+ " = " + " " + result +"<br />")
+
+
+// var num=2
+// var result=tab*num
+// document.write(tab + " x " + num+ " = " + " " + result +"<br />" )
+
+
+// var num=3
+// var result=tab*num
+// document.write(tab + " x " + num+ " = " + " " + result + "<br />" )
+
+
+// var num=4
+// var result=tab*num
+// document.write(tab + " x " + num+ " = " + " " + result + "<br />" )
+
+
+// var num=5
+// var result=tab*num
+// document.write(tab + " x " + num+ " = " + " " + result + "<br />" )
+
+// var num=6
+// var result= tab*num
+// document.write(tab + " x " + num+ " = " + " " + result +"<br />")
+
+// var num=7
+// var result= tab*num
+// document.write(tab + " x " + num+ " = " + " " + result +"<br />")
+
+// var num=8
+// var result= tab*num
+// document.write(tab + " x " + num+ " = " + " " + result +"<br />")
+
+// var num=9
+// var result= tab*num
+// document.write(tab + " x " + num+ " = " + " " + result +"<br />")
+
+// var num=10
+// var result= tab*num
+// document.write(tab + " x " + num+ " = " + " " + result +"<br />")
+
+
+// // Qno#6
+
+// var c = 16
+// var f = 59
+// c = (f - 32) * 5/9
+// f = (c * 9/5) + 32
+// document.write(  c +"°C" + " " + "is" + " " + f + "°F" +"<br />" )
+// document.write(  f +"°F" + " " + "is" + " " + c + "°C")
+
+
+// // Qno#7
+
+// document.write("<h1>Shopping Cart</h1>")
+
+// var item1 = 650
+// var qI1= 3
+// var item2 = 100
+// var qI2 = 7
+// var shipping = 100
+// var result1 = item1*qI1
+// var result2 = item2*qI2
+// var total = result1 + result2 + shipping
+// document.write("price of item 1 is" +" "+ item1 +"<br />")
+// document.write("quantity of item 1 is"+" "+ qI1 +"<br />")
+// document.write("price of item 2 is" + " " + item2 +"<br />")
+// document.write("Quantity of item 2 is" +" "+ qI2  +"<br />")
+// document.write("shipping charges" + shipping +"<br />")
+
+// document.write("The total cost of your order is" + " " + total )
+
+
+// // Qno#8
+
+// document.write("<h1>Mark Sheet</h1>")
+// var tm = 980
+// var om = 804
+// var per =  om/tm* 100
+// document.write("Total marks:" +" " + tm +"<br />")
+// document.write("obtained marks:" + " " + om +"<br />")
+// document.write("percentage:" +" " + per + "%" +"<br />")
+
+// // Qno#9
+
+// document.write("<h1>Currency to PKR</h1>")
+// var usd = 270
+// var riyal = 74
+// var total = (usd*10) + (riyal*25) 
+// document.write("Total currency in PKR is:" + " " + total)
+
+// // Qno#10
+
+// var num = 5
+// console.log((num*10) / 2)
+
+// // Qno#11
+
+// document.write("<h1>Age calculator</h1>")
+
+// var currentYear = 2023
+// var birthYear = 1999
+// var age = currentYear - birthYear
+// document.write("current year is:" + " " + currentYear + "<br />" + "Birth year:" + " " + birthYear + "<br />" + "My age is :" + " " + age)
+
+
+// // Qno#12
+
+// document.write("<h1>The Geometrics</h1>")
+
+
+// var radius = 20
+// var cer = (radius * 2 * Math. PI)
+// var area = ( radius * radius * Math. PI)
+
+// document.write("radius of circle"+" "+ radius +  "<br />")
+// document.write("The circumference is :" +" "+ cer +  "<br />")
+// document.write( "The area is:"+" " + area)
+
+
+
+
+
+
+
+
+
+
+
+
+// // Qno#13
+ 
+// document.write("<h1> The lifetime supply caclulator</h1>")
+
+// var favSnack = "freshup"
+// var currAge = 24
+// var estAge = 63
+// var perDay = 3
+// var lifeTime = (estAge - currAge) * 356 * 3
+// document.write("favorite snaks:" + " " + favSnack + "<br />" + "current age:" + " " + currAge + "<br />" + "estimated maximum :" + " " + estAge + "<br />" + "you will need"+" " + lifeTime +" "+ favSnack +" "+ "to last you until the ripe old age of" +" "+ estAge)
+
